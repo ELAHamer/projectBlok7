0bef55c0a0bd14ab65fd3eaf26c1d9f8e769be7c
hanskaaskan <ernst.hamer@casema.nl>
2017-03-31 17:13:12 +0200

    Update ORF.java

--- project7a/src/ORF.java
+++ project7a/src/ORF.java
@@ -1,19 +1,28 @@
-/**
- *
- * @author Rob
+/** 
+* Functie: ORF object voor opslag van de ORFs
+ * 
+ * @author Koen Rademaker, Ernst Hamer, Rob van Deelen
+ * @Creation date: 21/03/2017
+ * @version 4.0
+ * @known bugs: none
  */
 public class ORF extends DNA {
     private Integer frame;
     private Integer positie;
     
-    
+/**
+ * constructor DNA
+ */
     public ORF(String seq, Float pcN, Integer pos,Integer frm) {
         super(seq, pcN);
         this.positie = pos;
         this.frame = frm;
     }
-    
-    @Override
+/**
+     * bepaalt op basis van de sequentie het percentage onbekende nucleotiden
+     * @param seq
+     * @return percentage
+     */
     public Float bepaalPercentageN(String seq){
         int countN=0;
         
@@ -21,21 +30,39 @@ public class ORF extends DNA {
             if (seq.charAt(i) == 'N');              
             countN++;
             }
-        
-        Float pcN = ((float)countN/(float)seq.length())*100;
+        Float pcN = ((float)countN/(float)seq.length())*100;             
         return pcN;
     }
     
-    public Integer getFrame(){
-        return this.frame;
+    /**
+     * 
+     * @return percentage
+     */
+    public Float getPercentageN(){
+        return this.PercentageN;
     }
-    public void setFrame(Integer frm){
-        this.frame = frm;
+
+    /**
+     *
+     * @return sequentie
+     */
+    public String getSequentie(){
+        return this.sequentie;
     }
-    public void setPositie(Integer pos){
-        this.positie = pos;
+
+    /**
+     * schrijft sequentie over
+     * @param seq
+     */
+    public void setSequentie(String seq){
+        this.sequentie = seq;      
     }
-    public Integer getPositie(){
-        return this.positie;
+
+    /**
+     * schrijft percentage over
+     * @param pcN
+     */
+    public void setPercentageN(Float pcN){
+        this.PercentageN = pcN;
     }
 }
