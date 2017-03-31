ac441def7cd1500460e607197fc4104ee75c5d85
hanskaaskan <ernst.hamer@casema.nl>
2017-03-31 17:09:02 +0200

    Update DNA.java

--- project7a/src/DNA.java
+++ project7a/src/DNA.java
@@ -1,16 +1,28 @@
-/**
- *
- * @author Rob
+/** 
+ * Functie: DNA object voor opslag van de DNA sequenties
+ * 
+ * @author Koen Rademaker, Ernst Hamer, Rob van Deelen
+ * @Creation date: 21/03/2017
+ * @version 4.0
+ * @known bugs: none
  */
 public class DNA {
     protected String sequentie;
     protected Float PercentageN;
         
+    /**
+     * constructor DNA
+     */
     DNA(String seq,Float pcN) {
         this.PercentageN = bepaalPercentageN(seq);
         this.sequentie = seq;
     }
     
+    /**
+     * bepaalt op basis van de sequentie het percentage onbekende nucleotiden
+     * @param seq
+     * @return percentage
+     */
     public Float bepaalPercentageN(String seq){
         int countN=0;
         
@@ -22,15 +34,34 @@ public class DNA {
         return pcN;
     }
     
+    /**
+     * 
+     * @return percentage
+     */
     public Float getPercentageN(){
         return this.PercentageN;
     }
+
+    /**
+     *
+     * @return sequentie
+     */
     public String getSequentie(){
         return this.sequentie;
     }
+
+    /**
+     * schrijft sequentie over
+     * @param seq
+     */
     public void setSequentie(String seq){
         this.sequentie = seq;      
     }
+
+    /**
+     * schrijft percentage over
+     * @param pcN
+     */
     public void setPercentageN(Float pcN){
         this.PercentageN = pcN;
     }
